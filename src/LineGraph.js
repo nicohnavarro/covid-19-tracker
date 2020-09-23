3f23156e1f1f193224d59f0a4b0b634a209f8296
nicohnavarro <nicohnavarro@gmail.com>
2020-09-23 00:11:10 -0300

    modify chart

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Line } from 'react-chartjs-2';
-
+import numeral from 'numeral'
 
 const options = {
     legend: {
@@ -47,7 +47,7 @@ const options = {
 }
 
 
-function LineGraph() {
+function LineGraph({casesType ='cases'}) {
     const [data, setData] = useState({});
     // http://disease.sh/v3/covid-19/historical/all?lastdays=120
 
@@ -81,18 +81,20 @@ function LineGraph() {
 
     return (
         <div>
-            <Line
-                options={options}
-                data={{
-                    datasets: [
-                        {
-                            backgroundColor: "rgba(204, 16, 52, 0.5",
-                            borderColor: "#CC1034",
-                            data: data
-                        },
-                    ]
-                }}
-            />
+            {data?.length > 0 && (
+                <Line
+                    options={options}
+                    data={{
+                        datasets: [
+                            {
+                                backgroundColor: "rgba(204, 16, 52, 0.5",
+                                borderColor: "#CC1034",
+                                data: data
+                            },
+                        ]
+                    }}
+                />
+            )}
         </div>
     )
 }
