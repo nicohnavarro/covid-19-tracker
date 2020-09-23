7cb20f41432d34a9da6b4aa968596e22948015eb
nicohnavarro <nicohnavarro@gmail.com>
2020-09-22 23:55:59 -0300

    add chart

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { MenuItem, FormControl, Select, Card, CardContent } from "@material-ui/c
 import InfoBox from './InfoBox';
 import Map from './Map';
 import Table from './Table';
+import { sortData } from './util';
+import LineGraph from './LineGraph';
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -38,7 +40,8 @@ function App() {
               name: country.country,
               value: country.countryInfo.iso2
             }));
-          setTableData(data);
+            const sortedData = sortData(data);
+          setTableData(sortedData);
           setCountries(countries);
         });
     };
@@ -72,6 +75,7 @@ function App() {
           <h3>Live Cases by Country</h3>
           <Table countries={tableData}></Table>
           <h3>Worldwide new cases</h3>
+          <LineGraph></LineGraph>
         </CardContent>
 
       </Card>
---
+++ src/LineGraph.js
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from 'react'
+import { Line } from 'react-chartjs-2';
+
+
+const options = {
+    legend: {
+        display: false,
+    },
+    elements: {
+        point: {
+            radius: 0,
+        },
+    },
+    maintainAspectRatio: false,
+    tooltips: {
+        mode: "index",
+        intersect: false,
+        callbacks: {
+            label: function (tooltipItem, data) {
+                return numeral(tooltipItem.value).format("+0,0");
+            },
+        },
+    },
+    scales: {
+        xAxes: [
+            {
+                type: "time",
+                time: {
+                    format: "MM/DD/YY",
+                    tooltipFormat: "ll",
+                },
+            },
+        ],
+        yAxes: [
+            {
+                gridLines: {
+                    display: false,
+                },
+                ticks: {
+                    callback: function (value, index, values) {
+                        return numeral(value).format("0a");
+                    },
+                },
+            },
+        ],
+    },
+}
+
+
+function LineGraph() {
+    const [data, setData] = useState({});
+    // http://disease.sh/v3/covid-19/historical/all?lastdays=120
+
+    const buildChartData = (data, casesType = 'cases') => {
+        const chartData = [];
+        let lastDataPoint;
+        for (let date in data.cases) {
+            if (lastDataPoint) {
+                const newDataPoint = {
+                    x: date,
+                    y: data[casesType][date] - lastDataPoint
+                }
+                chartData.push(newDataPoint);
+            }
+            lastDataPoint = data['cases'][date];
+        }
+        return chartData;
+    };
+
+    useEffect(() => {
+        const fecthData = async () => {
+            await fetch('http://disease.sh/v3/covid-19/historical/all?lastdays=120')
+                .then(response => response.json())
+                .then(data => {
+                    const chartData = buildChartData(data);
+                    setData(chartData);
+                });
+        };
+        fecthData();
+    }, [])
+
+    return (
+        <div>
+            <Line
+                options={options}
+                data={{
+                    datasets: [
+                        {
+                            backgroundColor: "rgba(204, 16, 52, 0.5",
+                            borderColor: "#CC1034",
+                            data: data
+                        },
+                    ]
+                }}
+            />
+        </div>
+    )
+}
+
+export default LineGraph
